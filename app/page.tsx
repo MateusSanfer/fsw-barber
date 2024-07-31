33635e9cf77bd11314e79f52084b2092de335859
mateussanfer <mateus.sanfer@gmail.com>
2024-07-30 23:46:56 -0300

    chore: add prettier tailwindcss plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "./_components/ui/button";
+"use client"
+
+import { useState } from "react"
 
 const Home = () => {
-  return <Button>test</Button>;
-};
+  const [] = useState()
+  return <h1 className="text-red-500">red text</h1>
+}
 
-export default Home;
+export default Home
